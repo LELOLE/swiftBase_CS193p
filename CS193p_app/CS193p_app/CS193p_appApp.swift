870334a6c5a15cf6fa3fb10c46c0fde648e24ef2
LELOLE <azhu718050848@163.com>
2026-01-25 23:53:49 +0800

    New Project

---
+++ CS193p_app/CS193p_app/CS193p_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_appApp.swift
+//  CS193p_app
+//
+//  Created by li zhujun on 2026/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p_app/CS193p_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CS193p_app
+//
+//  Created by li zhujun on 2026/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
